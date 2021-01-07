96e5813463a5b3a231923bfcaf81adecc07db6ee
Davide <davide.zanella10@gmail.com>
2021-01-07 22:30:08 +0100

    Fixed obstacles offset and merged together overlapping obstacles

--- src/planning/planning.cpp
+++ src/planning/planning.cpp
@@ -16,21 +16,56 @@ namespace student {
         return newPoly;
     }
     
-    void expandPolygon(Polygon polygon, Polygon* result) {
-    printf("expandPolygon\n");
+    void expandPolygon(Polygon &polygon) {
         ClipperLib::Path p;
         ClipperLib::Paths solution;
-        
-        for (Point pt : polygon)
-            p << ClipperLib::IntPoint(pt.x*OBJECTS_SCALE_FACTOR, pt.y*OBJECTS_SCALE_FACTOR);
+
+        for (Point pt : polygon){
+            p << ClipperLib::IntPoint(pt.x * OBJECTS_SCALE_FACTOR, pt.y * OBJECTS_SCALE_FACTOR);
+        }
         
         ClipperLib::ClipperOffset co;
         co.AddPath(p, ClipperLib::jtSquare, ClipperLib::etClosedPolygon);
-        co.Execute(solution, 5*OBJECTS_SCALE_FACTOR);
-        
+        co.Execute(solution, 15 * OBJECTS_SCALE_FACTOR);
+
+        polygon.clear();
         for (ClipperLib::Path sol : solution)
             for (ClipperLib::IntPoint pt : sol)
-                (*result).emplace_back(pt.X/OBJECTS_SCALE_FACTOR, pt.Y/OBJECTS_SCALE_FACTOR);
+                polygon.emplace_back(pt.X / OBJECTS_SCALE_FACTOR, pt.Y / OBJECTS_SCALE_FACTOR);
+    }
+
+    void joinOverlappingPolygons(std::vector<Polygon> &polygons) {
+        for(int i = 0; i < polygons.size(); i++) {
+            for(int j = 0; j < polygons.size(); j++) {
+                if (i == j || polygons[i].size() == 0 || polygons[j].size() == 0)
+                    continue;
+                ClipperLib::Paths subj(1), clip(1), solution;
+
+                for (Point pt : polygons[i])
+                    subj[0] << ClipperLib::IntPoint(pt.x * OBJECTS_SCALE_FACTOR, pt.y * OBJECTS_SCALE_FACTOR);
+                for (Point pt : polygons[j])
+                    clip[0] << ClipperLib::IntPoint(pt.x * OBJECTS_SCALE_FACTOR, pt.y * OBJECTS_SCALE_FACTOR);
+
+                ClipperLib::Clipper c;
+                c.AddPaths(subj, ClipperLib::ptSubject, true);
+                c.AddPaths(clip, ClipperLib::ptClip, true);
+                c.Execute(ClipperLib::ctUnion, solution, ClipperLib::pftNonZero, ClipperLib::pftNonZero);
+
+                if(solution.size() == 1) {
+                    polygons[i].clear();
+                    polygons[j].clear();
+                    for (ClipperLib::IntPoint pt : solution[0])
+                        polygons[i].emplace_back(pt.X / OBJECTS_SCALE_FACTOR, pt.Y / OBJECTS_SCALE_FACTOR);
+                }
+            }
+        }
+
+        for (auto it = polygons.begin(); it != polygons.end();) {
+            if((*it).size() == 0)
+                polygons.erase(it);
+            else
+                ++it;
+        }
     }
 
     polygon_type_def convertPolygonToBoost(const Polygon &input) {
@@ -48,7 +83,7 @@ namespace student {
     }
 
     void drawGraphImage(const UndirectedGraph &graph, const std::vector<bp::voronoi_vertex<double>> &vertices,
-                        const Point &currPosition, const Point &targetPoint) {
+                        const Point &currPosition, const Point &targetPoint, const std::vector <Polygon> &obstacles) {
         // let's draw the result
         cv::Mat tmp(600, 800, CV_8UC3, cv::Scalar(255, 255, 255));
         int thickness = 2;
@@ -64,17 +99,25 @@ namespace student {
             cv::line(tmp, cvstart, cvend, cv::Scalar(0, 0, 0), thickness, lineType);
         }
 
+        for (auto &obst : obstacles){
+            for (int i = 1; i <= obst.size(); i++){
+                cv::Point cvstart = cv::Point(obst[i % obst.size()].x, obst[i % obst.size()].y);
+                cv::Point cvend = cv::Point(obst[i-1].x, obst[i-1].y);
+                cv::line(tmp, cvstart, cvend, cv::Scalar(0, 0, 255), thickness, lineType);
+            }
+        }
+
         cv::circle(tmp, cv::Point(currPosition.x, currPosition.y), 5, cv::Scalar(255, 0, 0), CV_FILLED, 8, 0);
         cv::circle(tmp, cv::Point(targetPoint.x, targetPoint.y), 5, cv::Scalar(0, 255, 0), CV_FILLED, 8, 0);
 
-        //auto pathImg = "/root/workspace/Graph.png";
-        //cv::imwrite(pathImg, tmp);
+        auto pathImg = "/root/workspace/Graph.png";
+        cv::imwrite(pathImg, tmp);
         //std::cout << "img saved at "<<pathImg << std::endl;
 
         
-        cv::namedWindow("Path Voronoi", cv::WINDOW_NORMAL);
-        cv::imshow("Path Voronoi", tmp);
-        cv::waitKey(0);
+        //cv::namedWindow("Path Voronoi", cv::WINDOW_NORMAL);
+        //cv::imshow("Path Voronoi", tmp);
+        //cv::waitKey(0);
     }
 
     bool elaborateVoronoi(const Polygon &borders, const std::vector <Polygon> &obstacle_list,
@@ -95,11 +138,9 @@ namespace student {
 
         // an array of all the possible objects present in the arena
         std::vector <Polygon> allObjects;
-        for (Polygon p : obstacle_list) {
-            //Polygon p2;
-            expandPolygon(p, &p);
+        for (Polygon p : obstacle_list)
             allObjects.push_back(p);
-        }
+
         for (auto victim : victim_list)
             allObjects.push_back(victim.second);
         allObjects.push_back(gate);
@@ -110,6 +151,12 @@ namespace student {
             toAvoid.erase(std::remove(toAvoid.begin(), toAvoid.end(), targetPoly));
             if (prevTarget.size() > 0)
                 toAvoid.erase(std::remove(toAvoid.begin(), toAvoid.end(), prevTarget));
+
+            for (int i = 0; i < toAvoid.size(); i++) {
+                expandPolygon(toAvoid[i]);
+            }
+            // merge together overlapping obstacles
+            joinOverlappingPolygons(toAvoid);
 
             polygon_type_def poly = convertPolygonToBoost(targetPoly);
 
@@ -134,7 +181,7 @@ namespace student {
             std::vector<int> d(num_vertices(graph));
             std::vector <vertex_descriptor> p(num_vertices(graph));
 
-            drawGraphImage(graph, vertices, currPosition, targetPoint);
+            drawGraphImage(graph, vertices, currPosition, targetPoint, toAvoid);
 
             // use Dijkstra to compute a distance vector from the start point
             boost::dijkstra_shortest_paths(graph, first, boost::predecessor_map(&p[0]).distance_map(&d[0]));
